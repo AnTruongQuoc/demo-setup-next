a57873782d1eb9990b5f08ed5d3ef8f35f0eef48
Bang Nguyen <bangtuyet2269@gmail.com>
2022-09-05 15:15:02 +0700

    Update exploration section & footer

--- src/components/NftExploration.tsx
+++ src/components/NftExploration.tsx
@@ -7,7 +7,7 @@ function CreateNFT() {
             <div className='exploration-section-right'></div>
             <div className='exploration-nft flex justify-center items-center flex-col h-min mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
                 <h1 className='font-bold text-white text-5xl'>Create Your Own NFT!</h1>
-                <p className='my-10 font-normal'>
+                <p className='my-10 font-normal text-center'>
                     We have a large scale group to support each other in this game, Join us to get the news as soon <br></br> as
                     possible and follow our latest announcements!
                 </p>
