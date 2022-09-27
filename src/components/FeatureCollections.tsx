7278a598d0db2b3f4a0b05d57729c239431daff2
QuanHuynh <quanhuynhminh2112@gmail.com>
2022-09-27 22:47:38 +0700

    responsive

--- src/components/FeatureCollections.tsx
+++ src/components/FeatureCollections.tsx
@@ -4,13 +4,13 @@ import CollectionCard from './Card/CollectionCard';
 const FeatureCollections = () => {
   return (
     <>
-      <div className='flex justify-between w-full mt-20 items-center'>
-        <h3 className='text-center font-bold text-5xl leading-section'>FeatureCollections</h3>
-        <button className='font-semibold border-2 border-primary hover:bg-primary hover:text-white rounded-full px-11 py-3 h-fit'>
+      <div className='flex justify-between w-full mt-20 items-center '>
+        <h3 className='text-center text-2xl font-bold sm:text-5xl sm:leading-section'>Feature Collections</h3>
+        <button className='font-semibold border-2 border-primary hover:bg-primary hover:text-white rounded-full px-6 sm:px-11 py-2 sm:py-3 h-fit'>
           View More
         </button>
       </div>
-      <div className='flex justify-between w-full mt-14'>
+      <div className='flex justify-between w-full mt-14 flex-wrap sm:flex:nowrap'>
         {mockLiveAuction.map((item, i: number) => (
           <CollectionCard key={i} {...item} />
         ))}
--- src/components/LiveAuctions.tsx
+++ src/components/LiveAuctions.tsx
@@ -5,12 +5,12 @@ const LiveAuctions = () => {
   return (
     <>
       <div className='flex justify-between w-full mt-20 items-center'>
-        <h3 className='text-center font-bold text-5xl leading-section'>Live Auctions</h3>
-        <button className='font-semibold border-2 border-primary hover:bg-primary hover:text-white rounded-full px-11 py-3 h-fit'>
+        <h3 className='text-center text-2xl font-bold sm:text-5xl sm:leading-section'>Live Auctions</h3>
+        <button className='font-semibold border-2 border-primary hover:bg-primary hover:text-white rounded-full px-6 sm:px-11 py-2 sm:py-3 h-fit'>
           View More
         </button>
       </div>
-      <div className='flex justify-between w-full mt-14'>
+      <div className='flex justify-between w-full mt-14 flex-wrap sm:flex:nowrap'>
         {mockLiveAuction.map((item, i: number) => (
           <NFTCard key={i} {...item} />
         ))}
